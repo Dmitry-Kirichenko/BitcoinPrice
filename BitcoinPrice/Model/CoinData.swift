dd183a173c8964045795c6a54f89229dab286ebf
Dmitry Kyrychenko <kirichenkodmitryy@gmail.com>
2020-12-07 23:10:44 +0300

    Create CoinManager, CoinData

---
+++ BitcoinPrice/Model/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  CoinData.swift
+//  BitcoinPrice
+//
+//  Created by Дмитрий Кириченко on 07.12.2020.
+//
+
+import Foundation
---
+++ BitcoinPrice/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  BitcoinPrice
+//
+//  Created by Дмитрий Кириченко on 07.12.2020.
+//
+
+import Foundation
